b92d7a35f3be5ab314f7d127c2b49c43d44fd6dd
kirubel kassahun <kirubelkassahun9@gmail.com>
2025-07-22 12:08:51 +0300

    undo on appserviceProvider

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    base: '/build/',
+    server: {
+      https: {},
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
