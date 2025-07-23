7de63be18e00aead8969565b137f9f35b54ffc6f
kirubel kassahun <kirubelkassahun9@gmail.com>
2025-07-23 12:09:53 +0300

    add qr-code cofig file

---
+++ config/qr-code.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'writer' => 'png',
+    'size' => 200,
+    'margin' => 0,
+    'round_block_size' => [
+        'mode' => \BaconQrCode\Renderer\RendererStyle\RendererStyle::ROUND_BLOCK_SIZE_MODE_MARGIN,
+        'size' => 'auto'
+    ],
+    'writer_options' => [],
+];
