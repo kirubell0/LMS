b434b661997b264778e7598f8aaedf4f610bce70
kirubel kassahun <kirubelkassahun9@gmail.com>
2025-07-24 10:39:42 +0300

    add pakages on nixpkgs

--- public/build/assets/input-error-lFu69rc6.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-BMW1Et2K.js";import{c as o}from"./app-logo-icon-DU2g-acF.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
